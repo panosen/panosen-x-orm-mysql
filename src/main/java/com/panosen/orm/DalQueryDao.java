f739ea164549311a6c4862f8657c73c37c880bf7
harriszhang <harriszhang@live.cn>
2023-05-27 00:05:02 +0800

    0.4.2 update DalQueryDao

--- src/main/java/com/panosen/orm/DalQueryDao.java
+++ src/main/java/com/panosen/orm/DalQueryDao.java
@@ -3,22 +3,15 @@ package com.panosen.orm;
 import com.panosen.codedom.mysql.builder.SelectSqlBuilder;
 import com.panosen.orm.mapper.Mapper;
 
+import java.io.IOException;
 import java.util.List;
 
 public class DalQueryDao {
 
     private final DalClient dalClient;
 
-    public DalQueryDao(DalClient dalClient) {
-        this.dalClient = dalClient;
-    }
-
-    public static <TEntity> List<TEntity> queryList(DalClient dalClient, SelectSqlBuilder selectSqlBuilder, Mapper<TEntity> mapper) throws Exception {
-        return DalClientExtension.selectList(dalClient, selectSqlBuilder, mapper);
-    }
-
-    public static <TEntity> TEntity querySingle(DalClient dalClient, SelectSqlBuilder selectSqlBuilder, Mapper<TEntity> mapper) throws Exception {
-        return DalClientExtension.selectSingle(dalClient, selectSqlBuilder, mapper);
+    public DalQueryDao(String dataSourceName) throws IOException {
+        this.dalClient = DalClientFactory.getClient(dataSourceName);
     }
 
     public <TEntity> List<TEntity> queryList(SelectSqlBuilder selectSqlBuilder, Mapper<TEntity> mapper) throws Exception {
